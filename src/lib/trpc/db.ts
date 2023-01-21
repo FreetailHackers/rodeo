56c415ffe436bd5dfe78fb44a157ac0cf4711176
Daniel Ting <danielzting@gmail.com>
2023-01-21 15:34:38 -0600

    Remove unnecessary call to Prisma connect

--- src/lib/trpc/db.ts
+++ src/lib/trpc/db.ts
@@ -1,8 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { building } from '$app/environment';
 
 const prisma = new PrismaClient();
-if (!building) {
-	await prisma.$connect();
-}
 export default prisma;
