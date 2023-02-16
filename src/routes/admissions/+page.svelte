a91e383a2fb32734421a72d0e630a1d153a963fe
Daniel Ting <danielzting@gmail.com>
2023-02-16 14:55:40 -0600

    Make accept/reject/waitlist in admissions sticky
    The decision buttons at the bottom of the admissions page now sticks to
    bottom as you scroll, making it a lot easier to use them on mobile.

--- src/routes/admissions/+page.svelte
+++ src/routes/admissions/+page.svelte
@@ -11,19 +11,34 @@
 {:else}
 	<h1>{data.user.fullName}</h1>
 	<User user={data.user} />
-	<form method="POST" use:enhance>
-		<input type="hidden" name="id" value={data.user.id} />
-		<button type="submit" formaction="?/accept">Accept</button>
-		<button type="submit" formaction="?/reject">Reject</button>
-		<button type="submit" formaction="?/waitlist">Waitlist</button>
-	</form>
+	<div id="form">
+		<div id="padding" />
+		<form method="POST" use:enhance>
+			<input type="hidden" name="id" value={data.user.id} />
+			<button type="submit" formaction="?/accept">Accept</button>
+			<button type="submit" formaction="?/reject">Reject</button>
+			<button type="submit" formaction="?/waitlist">Waitlist</button>
+		</form>
+	</div>
 {/if}
 
 <style>
+	#form {
+		position: sticky;
+		bottom: 0;
+	}
+
+	#padding {
+		height: 1rem;
+		background: linear-gradient(transparent, white);
+	}
+
 	form {
 		flex-direction: row;
 		justify-content: space-between;
 		gap: 1rem;
+		background: white;
+		padding-bottom: 1rem;
 	}
 
 	button {
