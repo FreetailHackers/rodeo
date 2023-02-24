f553098cbbf8927d2fea4ab25a7bea092380edcd
Daniel Ting <danielzting@gmail.com>
2023-02-23 20:46:53 -0600

    Make navbar mobile responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,17 +7,22 @@
 </script>
 
 <nav>
-	<a href="/">Home</a>
-	<a href="/schedule">Schedule</a>
-	<a href="/info">Info</a>
-	{#if data.user?.role === Role.HACKER}
-		<a href="/apply">Apply</a>
-		<!-- <a href="/id">My Hacker ID</a> -->
-	{:else if data.user?.role === Role.ADMIN}
-		<a href="users">Users</a>
-		<a href="admin">Admin</a>
-		<a href="admissions">Admissions</a>
-	{/if}
+	<label for="hamburgerCheckbox" id="hamburger">Menu</label>
+	<input type="checkbox" id="hamburgerCheckbox" style="display: none" />
+	<ul id="menu">
+		<li><a href="/">Home</a></li>
+		<li><a href="/schedule">Schedule</a></li>
+		<li><a href="/info">Info</a></li>
+		{#if data.user?.role === Role.HACKER}
+			<li><a href="/apply">Apply</a></li>
+			<!-- <li><a href="/id">My Hacker ID</a></li> -->
+		{:else if data.user?.role === Role.ADMIN}
+			<li><a href="/users">Users</a></li>
+			<li><a href="/admin">Admin</a></li>
+			<li><a href="/admissions">Admissions</a></li>
+		{/if}
+	</ul>
+	<hr />
 </nav>
 
 <slot />
@@ -32,6 +37,38 @@
 </footer>
 
 <style>
+	#menu {
+		display: none;
+		list-style: none;
+		padding: 0;
+	}
+
+	#hamburger {
+		display: block;
+		width: 100%;
+	}
+
+	#hamburgerCheckbox:checked + #menu {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	@media (min-width: 768px) {
+		#hamburger {
+			display: none;
+		}
+
+		#menu {
+			display: flex;
+		}
+
+		#menu li + li::before {
+			content: '|';
+			padding: 0.5rem;
+		}
+	}
+
 	hr {
 		margin-top: 1rem;
 	}
