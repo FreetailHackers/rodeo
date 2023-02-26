df3c9d6fa317e77ede2746d54456d1bec0bfca04
Daniel Ting <danielzting@gmail.com>
2023-02-25 22:36:53 -0600

    Make navbar stick to top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
 	import { Role } from '@prisma/client';
+	import { onMount } from 'svelte';
 	import type { LayoutData } from './$types';
 	import './global.css';
 
 	export let data: LayoutData;
+
+	let menu: HTMLUListElement;
+	let hamburgerCheckbox: HTMLInputElement;
+
+	onMount(() => {
+		for (const link of menu.childNodes) {
+			link.addEventListener('click', () => {
+				hamburgerCheckbox.checked = false;
+			});
+		}
+	});
 </script>
 
 <nav>
-	<label for="hamburgerCheckbox" id="hamburger">Menu</label>
-	<input type="checkbox" id="hamburgerCheckbox" style="display: none" />
-	<ul id="menu">
+	<label for="hamburgerCheckbox" id="hamburger"><b>MENU</b></label>
+	<input
+		type="checkbox"
+		id="hamburgerCheckbox"
+		bind:this={hamburgerCheckbox}
+		style="display: none"
+	/>
+	<ul id="menu" bind:this={menu}>
 		<li><a href="/">Home</a></li>
 		<li><a href="/schedule">Schedule</a></li>
 		<li><a href="/info">Info</a></li>
 		{#if data.user?.role === Role.HACKER}
-			<li><a href="/apply">Apply</a></li>
 			<!-- <li><a href="/id">My Hacker ID</a></li> -->
+			<li><a href="/apply">Apply</a></li>
 		{:else if data.user?.role === Role.ADMIN}
 			<li><a href="/users">Users</a></li>
 			<li><a href="/admin">Admin</a></li>
@@ -37,6 +54,14 @@
 </footer>
 
 <style>
+	nav {
+		position: sticky;
+		top: 0;
+		margin-top: 0;
+		background-color: white;
+		z-index: 999;
+	}
+
 	#menu {
 		display: none;
 		list-style: none;
@@ -46,12 +71,18 @@
 	#hamburger {
 		display: block;
 		width: 100%;
+		padding-top: 1rem;
 	}
 
 	#hamburgerCheckbox:checked + #menu {
 		display: flex;
 		flex-direction: column;
-		gap: 1rem;
+	}
+
+	a {
+		display: block;
+		width: 100%;
+		padding: 0.75rem 0;
 	}
 
 	@media (min-width: 768px) {
@@ -60,12 +91,19 @@
 		}
 
 		#menu {
+			margin: 0;
+			padding-top: 1rem;
 			display: flex;
 		}
 
 		#menu li + li::before {
 			content: '|';
 			padding: 0.5rem;
+		}
+
+		a {
+			display: inline;
+			width: initial;
 		}
 	}
 
