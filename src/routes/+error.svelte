cb80ff956ff6570d5855737e4796dd322ca9beb8
Jeongwoo CHoi <jeongwoo@utexas.edu>
2023-02-26 18:39:06 -0600

    Improve 401 error message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { page } from '$app/stores';
+	let message = '';
+	if ($page.error) {
+		message = $page.error.message;
+	}
+</script>
+
+<h1>{$page.status}</h1>
+{#if $page.status === 401}
+	<p>{message ? message : 'Unauthorized'}</p>
+	<p>
+		Please make sure you've logged into your account before accessing this page. If you still run
+		into issues, please contact <a href="mailto:tech@freetailhackers.com"
+			>tech@freetailhackers.com
+		</a>
+	</p>
+{:else}
+	<p>{message}</p>
+{/if}
