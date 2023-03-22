b3db56153a0e7fcc6bfd24e9de7354115bb1675d
Daniel Ting <danielzting@gmail.com>
2023-03-21 23:48:16 -0500

    Try edge functions

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,9 @@
 import { trpc } from '$lib/trpc/router';
+import type { Options } from '@sveltejs/adapter-vercel';
+
+export const config: Options = {
+	edge: true,
+};
 
 export const load = async ({ cookies }) => {
 	return { user: await trpc(cookies).users.get() };
