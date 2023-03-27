820f098dd558b7283b735918a2abb18b1dcd6c07
ChristinaGo <christinago@utexas.edu>
2023-03-26 21:30:58 -0500

    make homepage text editable with markdown and preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,22 @@
 	import { enhance } from '$app/forms';
 	import Announcements from '$lib/components/announcements.svelte';
 	import { Role } from '@prisma/client';
+	import SvelteMarkdown from 'svelte-markdown';
 
 	export let data;
 
 	let email: string;
+
+	let originalText =
+		'Thanks for your interest in our upcoming hackathon! **Hack The Future** will occure from' +
+		' **March 3rd, Friday evening until March 4th, Saturday evening**. Check our' +
+		' [website](https://freetailhackers.com/hack-the-future) and [schedule](schedule) for more details!';
+
+	let displayText = originalText;
+	let inputText = '';
+	function updateText() {
+		displayText = inputText;
+	}
 </script>
 
 {#if data.user}
@@ -13,12 +25,29 @@
 		Welcome to Rodeo{#if data.user.preferredName !== null && data.user.preferredName !== ''}, {data
 				.user.preferredName}{/if}!
 	</h1>
-	<p>
-		Thanks for your interest in our upcoming hackathon! <b>Hack The Future</b> will occur from
-		<b>March 3rd, Friday evening until March 4th, Saturday evening</b>. Check our
-		<a href="https://freetailhackers.com/hack-the-future">website</a>
-		and <a href="schedule">schedule</a> for more details!
-	</p>
+
+	<SvelteMarkdown source={displayText} />
+
+	<h4>Start typing to see preview:</h4>
+	<SvelteMarkdown source={inputText} />
+	{#if data.user?.role === Role.ADMIN}
+		<form>
+			<textarea
+				id="info"
+				placeholder="Modify the current homepage text..."
+				name="info"
+				rows="100"
+				bind:value={inputText}
+			/>
+			<button on:click={updateText} type="submit">Update Homepage Text</button>
+		</form>
+	{/if}
+	<h6>
+		FORMAT EXAMPLES FOR HOMEPAGE TEXT: *italics* **bold** ***bold italic*** ~~strikethrough~~
+		\`inline code snippet\` [I am a link!](https://example.com) ![Image alt
+		text](https://example.com/image.png) > Blockquote
+	</h6>
+
 	<p>
 		Please make sure to fill out the <a href="apply">application</a> as early as possible. Admission
 		will operate on a <b>first-come, first-serve basis</b>. Spots are limited.
@@ -27,16 +56,6 @@
 		If you have any questions or concerns, please contact
 		<a href="mailto:hello@freetailhackers.com">hello@freetailhackers.com</a>.
 	</p>
-
-	<!-- Customizable homepage text panel -->
-	{#if data.user.role === Role.ADMIN}
-		<h2>Homepage Text</h2>
-		<form method="POST" action="?/" use:enhance>
-			<textarea name="customHomeText" placeholder="Modify the current homepage text..." required />
-			<button>Modify</button>
-		</form>
-		<br />
-	{/if}
 
 	<!-- Admin announcements panel -->
 	<h2>Announcements</h2>
@@ -81,5 +100,8 @@
 <style>
 	#logout {
 		margin-top: 1rem;
+	}
+	h4 {
+		border-top: 1px solid black;
 	}
 </style>
