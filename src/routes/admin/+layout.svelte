d96b095e55ee20e9aabddb73c97e4758250bbdf0
nikshitak <nikkikurva@gmail.com>
2023-08-17 23:35:09 -0500

    clean up admin panel + phone

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -9,8 +9,6 @@
 	import Loader from '$lib/components/loader.svelte';
 	import { beforeNavigate, afterNavigate } from '$app/navigation';
 
-	export let data;
-
 	// Automatically display a toast if a form action returns a string
 	$: if (typeof $page.form === 'string') {
 		toasts.notify($page.form);
