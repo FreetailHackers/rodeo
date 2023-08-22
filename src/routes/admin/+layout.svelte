148ca9511e3fbfe56497cd3bf8e764caec37be4f
nikshitak <nikkikurva@gmail.com>
2023-08-21 23:19:04 -0500

    removed even more css

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -7,7 +7,6 @@
 <h1>Admin Panel</h1>
 
 <nav>
-	<label for="administration" id="hamburger"><b>MENU</b></label>
 	<input type="checkbox" id="administration" bind:this={hamburgerCheckbox} style="display: none" />
 	<ul>
 		<li><a href="/admin" class:active={$page.url.pathname === '/admin'}>Admin Settings</a></li>
@@ -31,41 +30,12 @@
 	ul {
 		list-style: none;
 		padding: 0;
-		transition: all 0.5s ease-out;
-		max-height: 0;
-		overflow: hidden;
-	}
-
-	#administration:checked + ul {
-		display: flex;
-		flex-direction: column;
-		max-height: 100vh;
 	}
 
 	ul a {
-		display: block;
-		padding: 0.7rem 0;
-	}
-
-	@media (min-width: 768px) {
-		#hamburger {
-			display: none;
-		}
-
-		ul {
-			display: flex;
-			max-height: fit-content;
-		}
-
-		ul li + li::before {
-			content: '|';
-			padding: 0.5rem;
-		}
-
-		ul a {
-			display: inline;
-			width: initial;
-		}
+		display: inline;
+		width: initial;
+		padding-top: 100px;
 	}
 
 	.active {
