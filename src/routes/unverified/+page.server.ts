c529db491aec6a22beefee16119780ec12abfbbc
Christina Go <christinago@utexas.edu>
2023-09-25 17:32:07 -0500

    accidently deleted files

---
+++ src/routes/unverified/+page.server.ts
@@ -0,0 +1,16 @@
+import { trpc } from '$lib/trpc/router';
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals }) => {
+	const session = await locals.auth.validate();
+	if (session == null || session.user.status !== 'CREATED') {
+		throw redirect(303, '/');
+	}
+};
+
+export const actions = {
+	default: async ({ locals }) => {
+		await trpc(locals.auth).users.sendVerificationEmail();
+		return 'Verification email resent!';
+	},
+};
---
+++ src/routes/unverified/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
+<svelte:head>
+	<title>Rodeo | Unverified Email</title>
+</svelte:head>
+
+<p>
+	You must verify your email before accessing this page. Please check your inbox for a verification
+	link. If you did not get one (please remember to check spam), you can request a new one below.
+</p>
+
+<form method="POST" use:enhance>
+	<button type="submit">Resend Verification Email</button>
+</form>
