1ed3dc0b15cf73ab3eea75291b21ec39f85c6367
qyuo <ale.pabon04@gmail.com>
2024-02-02 21:32:07 -0600

    fix pipeline maybe 2.0?

--- prisma/migrations/20240203032552_rsvp_deadline/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `confirmBy` on the `Settings` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Settings" DROP COLUMN "confirmBy",
-ADD COLUMN     "daysToRSVP" INTEGER;
