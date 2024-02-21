682c5284a600c4584c613e4314bd54291e629dfb
BrianTruong23 <truongthoithang@gmail.com>
2024-02-21 17:21:51 -0600

    squash-migration

--- prisma/migrations/20240217213949_black_listed_user_creation/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "AuthUser" ADD COLUMN     "blackListed" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240218233030_drop_black_listed/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `blackListed` on the `AuthUser` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "AuthUser" DROP COLUMN "blackListed";
