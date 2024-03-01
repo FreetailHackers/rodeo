077689116b2451dfa50ccea5bf137633a7890729
BrianTruong23 <truongthoithang@gmail.com>
2024-02-29 23:04:19 -0600

    updated hacker id

--- src/routes/id/+page.svelte
+++ src/routes/id/+page.svelte
@@ -25,112 +25,96 @@
 		<div class="text"><span>My Hacker ID</span></div>
 	</div> -->
 
+	<div class = "vertical-text"></div>
 
-	<div class = "vertical-text">
-	</div>
-
-	<div class = "driver-scan-div">
-		<div class = "driver-div">
-			<h1 class = "driver-div-text black">Driver </h1>
-			<h1 class = "driver-div-text red ">Name</h1>
-			<div class = "canvas">
+	<div class="driver-scan-div">
+		<div class="driver-div">
+			<h1 class="driver-div-text black">Driver</h1>
+			<h1 class="driver-div-text red ">Name</h1>
+			<div class="canvas">
 				<!-- <canvas bind:this={canvas} id="qrcode" /> -->
 			</div>
 		</div>
 
-		<div class = "ready-to-scan">
-			<h4 class = "ready-to-scan-text-ready black transform-vertical">Are you ready?</h4>
-			<h2 class = "ready-to-scan-text-scan red transform-vertical"> Show to Scan</h2>
+		<div class="ready-to-scan transform-vertical">
+			<h4 class="ready-to-scan-text-ready black ">Are you ready?</h4>
+			<h2 class="ready-to-scan-text-scan red">Show to Scan</h2>
 		</div>
-
 	</div>
-
 </div>
 
 <style>
-	/* .vertical-text {
-	writing-mode: vertical-rl;
-	transform: rotate(360deg);
-	} */
-	.canvas{
+	.canvas {
 		width: 300px;
-		height:300px;
-		background-color: #D9D9D9;
+		height: 300px;
+		background-color: #d9d9d9;
 	}
-	.driver-div{
+	.driver-div {
 		padding: 0.7rem 1.5rem 2rem;
-		box-shadow: 4px 4px 16px 0px #00000040; 
+		box-shadow: 4px 4px 16px 0px #00000040;
 		border-radius: 10px;
-
 	}
 
-	.driver-scan-div{
+	.driver-scan-div {
 		display: flex;
 		flex-direction: row;
 	}
-	.ready-to-scan{
+	.ready-to-scan {
 		display: flex;
-		flex-direction: row-reverse;
+		flex-direction: column;;
 		gap: 5px;
+		align-items: end;
+		justify-content: end;
+		font-family: "Geologica", sans-serif;
 	}
 	.ready-to-scan-text-ready {
 		display: flex;
-		align-items: center;
+		margin-left: -20px;
+		font-weight: normal;
+		margin-bottom: 30%;
+		font-family: "Geologica", sans-serif;
+
+	}
+	.ready-to-scan-text-scan {
+		font-weight: bold;
+		font-family: "Fugaz One", sans-serif;
 	}
 
-	.ready-to-scan-text-scan{
-
-	}
-
-	.transform-vertical{
+	.transform-vertical {
 		writing-mode: vertical-rl;
 		transform: rotate(360deg);
 	}
 
-	.ready-to-scan-text:first-of-type{
-		margin-bottom: -100px;
+	.black {
+		color: black;
+	}
+	.red {
+		color: var(--primary-accent);
 	}
 
-	.black{
-		color:var(--black)
-	}
-	.red{
-		color: var(--burnt-orange);
-	}
-
-	.driver-div-text{
-		font-family: "Fugaz One";
+	.driver-div-text {
+		font-family: 'Fugaz One';
 		text-align: left;
 	}
 
-	.driver-div-text:last-of-type{
+	.driver-div-text:last-of-type {
 		margin-top: -20px;
 	}
 
 	.vertical-text {
-		background-image: url("hackerid.svg");
-		background-size: cover; 
-		background-repeat: no-repeat; 
-		width: 14rem;
-		min-height: 200px ; 
-		height: 40rem;
+		background-image: url('hackerid.svg');
+		background-size: cover;
+		background-repeat: no-repeat;
+		width: 15rem;
+		min-height: 200px;
+		height: 60rem;
 		position: absolute;
-		top: 50%; 
-		left: 20%; 
-		transform: translate(-50%, -50%); 
+		top: 50%;
+		left: 20%;
+		transform: translate(-50%, -50%);
 	}
 
 
-
-	.text{
-		font-family: 'Zen Dots', sans-serif;
-		font-size: 4rem;
-		font-weight: 400;
-		line-height: 180px;
-		letter-spacing: 0em;
-		text-align: left;
-		position: relative;
-	}
 
 	.overall-container {
 		display: flex;
@@ -139,43 +123,7 @@
 		/* gap: 40px; */
 		justify-content: center;
 		align-items: center;
-
-	}
-
-
-	.purple-border {
-		display: flex;
 		flex-direction: column;
-		justify-content: flex-start;
-		align-items: center;
-		flex-wrap: nowrap;
-		height: 24rem;
-		border-radius: 28px;
-		background: #8b4f77;
-		width: fit-content;
-	}
-
-	.white-border {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		border-radius: 38px;
-		margin-top: 20px;
-		margin-left: 20px;
-		margin-right: 20px;
-		margin-bottom: 10px;
-		background: #f7f7ff;
-		width: fit-content;
-	}
-
-	span {
-		font-family: 'ruddy', sans-serif;
-		font-weight: 700;
-		font-style: normal;
-		color: white;
-		text-transform: uppercase;
-		font-size: xx-large;
-		line-height: normal;
 	}
 
 	canvas {
