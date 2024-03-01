fed4c02d4c1e163f59e97e833a44e1c4b7c7663d
Luis <luisalepabon@utexas.edu>
2024-03-01 04:34:24 +0000

    Resolve "Schedule"

--- src/routes/scan/+page.svelte
+++ src/routes/scan/+page.svelte
@@ -6,14 +6,16 @@
 	<title>Rodeo | Scan</title>
 </svelte:head>
 
-<section>
-	{#if data.scanActions.length === 0}
-		<i>No scan actions are configured. Add some in the <a href="/admin">admin panel</a>.</i>
-	{/if}
-	{#each data.scanActions as action}
-		<a href={'/scan/' + action}><button>{action}</button></a>
-	{/each}
-</section>
+<div class="main-content">
+	<section>
+		{#if data.scanActions.length === 0}
+			<i>No scan actions are configured. Add some in the <a href="/admin">admin panel</a>.</i>
+		{/if}
+		{#each data.scanActions as action}
+			<a href={'/scan/' + action}><button>{action}</button></a>
+		{/each}
+	</section>
+</div>
 
 <style>
 	section {
