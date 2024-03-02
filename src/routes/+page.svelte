73b3fdcbf3380f18b3786c1183181ef941c3db34
Nathan Cheng <nathanchengus@gmail.com>
2024-03-02 00:07:05 -0600

    Pls pls background work pls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,60 +18,60 @@
 	});
 </script>
 
-<div class="bg-img" />
-
 <svelte:head>
 	<title>Rodeo | Home</title>
 </svelte:head>
-<div class="container">
-	{#if data.user !== undefined}
-		<SvelteMarkdown source={data.settings.homepageText} />
+<div class="bg-img">
+	<div class="container">
+		{#if data.user !== undefined}
+			<SvelteMarkdown source={data.settings.homepageText} />
 
-		<form method="POST" action="?/logout" use:enhance>
-			<button type="submit" id="logout">Logout</button>
-		</form>
-		<!-- Admin announcements panel -->
-		<div class="header-container">
-			<h1 class="announcementHeader">ANNOUNCEMENTS</h1>
-		</div>
-		<Announcements announcements={data.announcements} admin={data.user.roles.includes('ADMIN')} />
-	{:else}
-		<!-- Signup page -->
-		{#if !data.canApply}
+			<form method="POST" action="?/logout" use:enhance>
+				<button type="submit" id="logout">Logout</button>
+			</form>
+			<!-- Admin announcements panel -->
+			<div class="header-container">
+				<h1 class="announcementHeader">ANNOUNCEMENTS</h1>
+			</div>
+			<Announcements announcements={data.announcements} admin={data.user.roles.includes('ADMIN')} />
+		{:else}
+			<!-- Signup page -->
+			{#if !data.canApply}
+				<p>
+					<b>
+						NOTE: Applications are closed. If you would like to be notified of future events, you
+						may enter your email below to subscribe to our mailing list.
+					</b>
+				</p>
+			{/if}
+			<h1>Login</h1>
+			<SocialLogin providers={data.providers} />
+
+			<form
+				method="POST"
+				action="?/login"
+				use:enhance={() => {
+					return async ({ update }) => {
+						update({ reset: false });
+					};
+				}}
+			>
+				<label for="email">Email</label>
+				<input id="email" name="email" required autocomplete="username" />
+				<label for="password">Password (<a href="/login/reset-password">forgot?</a>)</label>
+				<!-- HACK: Not required so we can easily log into test accounts lol -->
+				<input type="password" id="password" name="password" autocomplete="current-password" />
+				<button>Continue</button>
+			</form>
 			<p>
-				<b>
-					NOTE: Applications are closed. If you would like to be notified of future events, you may
-					enter your email below to subscribe to our mailing list.
-				</b>
+				Don't have an account yet? <a href="/register">Register here!</a>
 			</p>
+			<div class="header-container">
+				<h1 class="announcementHeader">ANNOUNCEMENTS</h1>
+			</div>
+			<Announcements announcements={data.announcements} admin={false} />
 		{/if}
-		<h1>Login</h1>
-		<SocialLogin providers={data.providers} />
-
-		<form
-			method="POST"
-			action="?/login"
-			use:enhance={() => {
-				return async ({ update }) => {
-					update({ reset: false });
-				};
-			}}
-		>
-			<label for="email">Email</label>
-			<input id="email" name="email" required autocomplete="username" />
-			<label for="password">Password (<a href="/login/reset-password">forgot?</a>)</label>
-			<!-- HACK: Not required so we can easily log into test accounts lol -->
-			<input type="password" id="password" name="password" autocomplete="current-password" />
-			<button>Continue</button>
-		</form>
-		<p>
-			Don't have an account yet? <a href="/register">Register here!</a>
-		</p>
-		<div class="header-container">
-			<h1 class="announcementHeader">ANNOUNCEMENTS</h1>
-		</div>
-		<Announcements announcements={data.announcements} admin={false} />
-	{/if}
+	</div>
 </div>
 
 <Schedule user={data.user} schedule={data.schedule} settings_timezone={data.settings.timezone} />
@@ -79,26 +79,15 @@
 <style>
 	.container {
 		padding: 0 20px;
+		display: center;
+		margin-left: auto;
+		margin-right: auto;
 	}
 	.bg-img {
-		/* Perfectly crisp at all zoom levels */
-		/* We can edit colors as we like by editing the .svg itself in VSCode */
 		background-color: #1d1d1c;
 		background-image: url('announcement-images/background.svg');
-
-		background-repeat: repeat;
-		background-size: cover;
-		filter: blur(0.05rem);
-
-		/* Absolute position so we can shift it to cover the whole screen */
-		position: fixed;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
-
-		/* display under all elements */
-		z-index: -1;
+		background-size: 110%;
+		min-height: 100vh;
 	}
 
 	input {
@@ -128,12 +117,12 @@
 		}
 		@media screen and (max-width: 768px) {
 			.announcementHeader {
-				font-size: 40px;
+				font-size: 30px;
 				line-height: 100px;
 			}
 			@media screen and (max-width: 480px) {
 				.announcementHeader {
-					font-size: 20px;
+					font-size: 15px;
 					line-height: 70px;
 				}
 			}
