69fff2b9c62a81aa208c4196fe16ab3392da842d
Michael Goppert <migopp@wireless-10-148-149-240.public.utexas.edu>
2024-03-03 09:36:50 -0600

    Squashed migrations

--- prisma/migrations/20240302204347_remove_other_categories_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `type` on the `OtherCategories` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "OtherCategories" DROP COLUMN "type";
