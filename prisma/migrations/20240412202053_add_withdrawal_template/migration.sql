2530462a434cf070f302d2e6e55efa89af483ed6
Annie Hu <anniegracehu@gmail.com>
2024-04-19 20:51:58 -0500

    Fixed prisma

--- prisma/migrations/20240412202053_add_withdrawal_template/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Settings" ADD COLUMN     "withdrawalWarningTemplate" TEXT NOT NULL DEFAULT 'You started editing your application but did not resubmit it so it is currently withdrawn.';
