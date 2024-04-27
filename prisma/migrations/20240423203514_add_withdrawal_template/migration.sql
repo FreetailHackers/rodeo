dcad14930f2f0c9736dc60e9f6a52e989031c6a6
Annie Hu <anniegracehu@gmail.com>
2024-04-27 04:38:56 +0000

    Resolve "Email receipt for withdrawing and editing application"

---
+++ prisma/migrations/20240423203514_add_withdrawal_template/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Settings" ADD COLUMN     "withdrawalWarningTemplate" TEXT NOT NULL DEFAULT 'We''ve noticed that your application has been withdrawn from our hackathon. If you''re still intending to submit, please ensure that you complete your edits and click ''submit''.';
