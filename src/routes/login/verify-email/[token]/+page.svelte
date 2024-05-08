ba2fc2746f90f7a33a4a8e8029f11b78a7dc7a4a
qyuo <ale.pabon04@gmail.com>
2024-05-08 10:49:51 -0500

    Basic mock-up

--- src/routes/login/verify-email/[token]/+page.svelte
+++ src/routes/login/verify-email/[token]/+page.svelte
@@ -6,37 +6,10 @@
 	<title>Rodeo | Verify Email</title>
 </svelte:head>
 
-<div class="topographic-background">
-	<div class="top-margin">
-		{#if data.success}
-			<p>Your email has been successfully verified! You can close this window now.</p>
-		{:else}
-			<p>This verification link either never existed or expired.</p>
-		{/if}
-	</div>
+<div class="main-content">
+	{#if data.success}
+		<p>Your email has been successfully verified! You can close this window now.</p>
+	{:else}
+		<p>This verification link either never existed or expired.</p>
+	{/if}
 </div>
-
-<style>
-	.topographic-background {
-		/* display: flex;
-		flex-direction: column;
-		align-items: center;
-		background-color: #303030;
-		background-image: url('/Topographic Background.svg');
-		background-size: 110%;
-		min-height: calc(100vh - 159px); */
-	}
-
-	.top-margin {
-		width: 75%;
-		align-items: center;
-		color: #f2ebd9;
-		margin-top: 4rem;
-	}
-
-	@media (max-width: 768px) {
-		.topographic-background {
-			min-height: calc(100vh - 56px);
-		}
-	}
-</style>
--- src/routes/unverified/+page.svelte
+++ src/routes/unverified/+page.svelte
@@ -6,7 +6,7 @@
 	<title>Rodeo | Unverified Email</title>
 </svelte:head>
 
-<div class="top-margin">
+<div class="main-content">
 	<p>
 		You must verify your email before accessing this page. Please check your inbox for a
 		verification link. If you did not get one (remember to check spam), you can request a new one
@@ -16,18 +16,3 @@
 		<button type="submit">Resend Verification Email</button>
 	</form>
 </div>
-
-<style>
-	.top-margin {
-		width: 75%;
-		align-items: center;
-		color: #f2ebd9;
-		margin-top: 4rem;
-	}
-
-	@media (max-width: 768px) {
-		.topographic-background {
-			min-height: calc(100vh - 56px);
-		}
-	}
-</style>
