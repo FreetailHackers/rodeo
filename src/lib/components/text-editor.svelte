5ff33be44774d28e84e174a2920c4bddd5adc082
Annie Hu <anniegracehu@gmail.com>
2024-05-27 19:11:23 -0700

    Display html as rendered always

--- src/lib/components/text-editor.svelte
+++ src/lib/components/text-editor.svelte
@@ -46,7 +46,7 @@
 		{:else}
 			<div class:announcement-font={useAnnouncementFont}>
 				{#if isHTML}
-					{value}
+					{@html value}
 				{:else}
 					<SvelteMarkdown source={value} />
 				{/if}
