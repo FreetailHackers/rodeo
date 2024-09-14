184ea00062c82d2929ba0af45a65aeb08f07e998
FlabbyToes <ahuliangbo@gmail.com>
2024-09-13 22:30:41 -0500

    settings ui

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -8,22 +8,21 @@
 <svelte:head>
 	<title>Rodeo | Hacker ID</title>
 </svelte:head>
-
 <div class="container">
 	<!-- Name and Email -->
 	<div class="user-info">
-		<label for="name">Name:</label>
+		<label class="form-heading" for="name">Name: </label>
 		<input type="text" id="name" value="Name Name" />
 
-		<label for="email">Email:</label>
+		<label class="form-heading" for="email">Email:</label>
 		<input type="email" id="email" value="namename@gmail.com" />
 	</div>
+	<div />
 
 	<!-- Dietary Restrictions -->
 	<hr />
-	<h3>Dietary Restrictions</h3>
 	<div class="dietary-restrictions">
-		<label for="dietary">Dietary Restrictions:</label>
+		<label class="form-heading" for="dietary">Dietary Restrictions</label>
 		<select id="dietary">
 			<option value="" disabled selected>Select one</option>
 			<option value="none">None</option>
@@ -37,9 +36,9 @@
 
 	<!-- Reset Password -->
 	<hr />
-	<h3>Reset Password</h3>
+	<h3 class="form-heading">Reset Password</h3>
 	<form method="POST" action="?/reset" use:enhance>
-		<label for="password">
+		<label class="form-heading" for="password">
 			<!-- svelte-ignore a11y-invalid-attribute -->
 			Enter a new password (<a href="javascript:;" on:click={() => (hidden = !hidden)}>
 				{#if hidden}show{:else}hide{/if}</a
@@ -51,32 +50,88 @@
 			name="password"
 			required
 			minlength="8"
+			placeholder="Old Password"
+		/>
+		<input
+			type={hidden ? 'password' : 'text'}
+			id="password"
+			name="password"
+			required
+			minlength="8"
+			placeholder="New Password"
 			autocomplete="new-password"
 		/>
 		<input type="hidden" name="token" value={$page.url.searchParams.get('token')} />
-		<button type="submit">Reset</button>
+		<div id="reset-password-buttons">
+			<button id="reset-password-buttons-cancel" type="reset">Cancel</button>
+			<button type="submit">Reset</button>
+		</div>
 	</form>
 </div>
 
 <style>
 	.container {
-		margin-left: 200px;
+		margin-left: 3%;
 		padding: 3rem;
 		background-color: #fff;
-		max-width: 50vw;
+		max-width: 40vw;
+	}
+	hr {
+		border: none;
+		border-top: 1px solid #bbbbbb;
+		margin-bottom: 1em;
+	}
+	label {
+		display: inline-block;
+		margin-bottom: 1em;
 	}
 
-	label {
-		margin-bottom: 0.5rem;
+	::placeholder {
+		color: #bbbbbb;
+	}
+
+	#reset-password-buttons {
+		display: flex;
+		justify-content: end;
+	}
+	#reset-password-buttons button {
+		margin-left: 1em;
+	}
+	#reset-password-buttons #reset-password-buttons-cancel {
+		background-color: #ffffff;
+		border-color: #7970ff;
+		border: 1px solid #7970ff;
+		color: #7970ff;
 	}
 
 	input,
 	select {
-		padding: 0.8rem;
-		border-radius: 8px;
-		border: 1px solid #ccc;
 		font-size: 1rem;
 		width: 100%;
+
+		height: 3em;
+		border-radius: 15px;
+		border: 1px solid #bbbbbb;
+		padding: 1em;
+	}
+	select {
+		appearance: none;
+		color: #bbbbbb;
+		padding: unset;
+		padding-left: 1em;
+		padding-right: 1em;
+		margin-bottom: 1em;
+
+		/* Arrow Icon Shit*/
+		background-image: url('selectionarrow.png');
+		background-repeat: no-repeat;
+		background-position: right center;
+		padding-right: 1em;
+		background-origin: content-box;
+	}
+
+	input {
+		margin-bottom: 1em;
 	}
 
 	@media only screen and (max-width: 767px) {
