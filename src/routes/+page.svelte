ceb6c27b395debfba7fdf68da6befa17e2bd9447
qyuo <ale.pabon04@gmail.com>
2024-09-14 20:27:21 -0500

    Fixed home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	// import SvelteMarkdown from 'svelte-markdown';
+	import SvelteMarkdown from 'svelte-markdown';
 	import { toasts } from '$lib/stores';
 	import { onMount } from 'svelte';
+	import Announcements from '$lib/components/announcements.svelte';
+
+	export let data;
 
 	// Some helpful error messages triggered in /src/lib/authenticate.ts
 	onMount(() => {
@@ -16,6 +19,33 @@
 <svelte:head>
 	<title>Rodeo | Home</title>
 </svelte:head>
+
+<div class="main-content">
+	<div>
+		<!-- svelte-ignore a11y-img-redundant-alt -->
+		<div class="homepage-text">
+			<SvelteMarkdown source={data.settings.homepageText} />
+		</div>
+	</div>
+
+	<div>
+		{#if data.settings.showAnnouncements}
+			{#if data.user !== undefined}
+				<!-- Admin announcements panel -->
+				<section id="Announcements">
+					<Announcements
+						announcements={data.announcements}
+						admin={data.user.roles.includes('ADMIN')}
+					/>
+				</section>
+			{:else}
+				<section id="Announcements">
+					<Announcements announcements={data.announcements} admin={false} />
+				</section>
+			{/if}
+		{/if}
+	</div>
+</div>
 
 <style>
 </style>
