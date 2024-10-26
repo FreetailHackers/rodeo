0980ee59fdcacdc4a0d8f9e81e9d8e91f00a522c
Nikshita Kurva <nikshitak@utexas.edu>
2024-10-26 18:13:41 +0000

    Resolve "Redo email sending functionality"

--- src/lib/trpc/routes/admissions.ts
+++ src/lib/trpc/routes/admissions.ts
@@ -1,7 +1,7 @@
 import type { Prisma, Status } from '@prisma/client';
 import { z } from 'zod';
 import { prisma } from '../db';
-import { sendEmails } from '../email';
+import { sendEmail } from '../email';
 import { authenticate } from '../middleware';
 import { t } from '../t';
 import { getSettings } from './settings';
@@ -63,11 +63,10 @@ async function releaseDecisions(ids?: string[]): Promise<void> {
 		} else if (decision === 'WAITLISTED') {
 			isHTML = (await getSettings()).waitlistIsHTML;
 		}
-		await sendEmails(
-			decisions.map((hacker) => hacker.authUser.email),
-			'Freetail Hackers status update',
-			template,
-			isHTML
+		await Promise.all(
+			decisions.map((hacker) =>
+				sendEmail(hacker.authUser.email, 'Freetail Hackers status update', template, isHTML)
+			)
 		);
 	}
 }
