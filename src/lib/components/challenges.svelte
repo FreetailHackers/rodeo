51e7486eaeae257c2997dd31472ddd926ea5db69
khang <khang.h.t@utexas.edu>
2025-01-08 17:38:16 -0600

    Redo Challenges layout and style

--- src/lib/components/challenges.svelte
+++ src/lib/components/challenges.svelte
@@ -11,13 +11,12 @@
 			<div class="prize-card">
 				<h3 id="prize-title">{challenge.title}</h3>
 
-				<div class="prize-name">
-					{#if challenge.prize}
-						<h5>{challenge.prize}</h5>
-					{:else}
-						<h5>Prize: Something Amazing</h5>
-					{/if}
-				</div>
+				{#if challenge.prize}
+					<h5>{challenge.prize}</h5>
+				{:else}
+					<h5>Prize: Something Amazing</h5>
+				{/if}
+
 				<p>{challenge.description}</p>
 			</div>
 		{/each}
@@ -26,54 +25,31 @@
 
 <style>
 	h1 {
-		font-family: var(--font-family);
-		display: flex;
-		justify-content: center;
-		font-size: 48px;
+		text-align: center;
 	}
 
 	h3 {
-		font-family: var(--font-family);
-		color: var(--accent-color);
 		text-align: center;
-		/* Header 3 */
-		font-size: 28px;
-		font-style: normal;
-		font-weight: 700;
-		line-height: normal;
-		text-transform: uppercase;
-		margin-bottom: 2px;
+		margin-bottom: 8px;
 	}
 
 	h5 {
-		font-family: var(--font-family);
-		color: var(--accent-color);
-		text-transform: capitalize;
-		font-size: 20px;
-		font-style: normal;
-		font-weight: 700;
-		line-height: normal;
 		text-align: center;
-		margin-top: 2px;
-		margin-bottom: 16px;
+		color: var(--secondary-color-1);
+		margin: 4px;
 	}
 
 	.container {
-		display: flex;
-		justify-content: center;
-		align-items: flex-end;
-		align-content: flex-end;
-		gap: 38px 39px;
-		flex-wrap: wrap;
+		columns: 20rem;
+		gap: 30px;
 	}
 
 	.prize-card {
-		display: flex;
-		flex-direction: column;
-		width: 30%;
-		border: 3px solid var(--accent-color);
-		border-radius: 20px;
-		padding: 0 25px 25px 25px;
+		break-inside: avoid;
+		margin-bottom: 50px;
+		border: 3px solid var(--accent);
+		border-radius: var(--border-radius);
+		padding: 0 1.5em 1.5em 1.5em;
 	}
 
 	@media (max-width: 768px) {
