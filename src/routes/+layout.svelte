b1e05a2b95ab045461b7f3644c182457ae36dd3a
nikshitak <nikshitak@utexas.edu>
2025-08-15 23:48:47 -0500

    fixed logout and changed hacktx website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -58,7 +58,7 @@
 			style="display: none"
 		/>
 		<menu id="menu" bind:this={menu}>
-			<li><a href="https://recordhacks.freetailhackers.com/">Homepage</a></li>
+			<li><a href="https://hacktx.com">Homepage</a></li>
 			<li><a href="/">Announcements</a></li>
 			{#if data.user?.roles.includes('HACKER')}
 				<li>
