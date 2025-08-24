682e4f1f9966e4d1e1ced5ed52da2f3633a442ae
nikshitak <nikshitak@utexas.edu>
2025-08-24 00:34:18 -0500

    remove unused argon2 import from authenticate.ts

--- src/lib/authenticate.ts
+++ src/lib/authenticate.ts
@@ -1,6 +1,5 @@
 import type { Role, AuthSession, AuthUser } from '.prisma/client';
 import { redirect } from '@sveltejs/kit';
-import argon2 from 'argon2';
 import type { RequestEvent } from '@sveltejs/kit';
 import { prisma } from './trpc/db';
 import { GitHub, Google } from 'arctic';
