6ef700be29ba2c04813a3a496061df0bdc281292
nikshitak <nikshitak@utexas.edu>
2025-08-26 00:28:33 -0500

    squashed commits

--- prisma/migrations/20250826052822_application_overhaul/migration.sql
+++ prisma/migrations/20250826052822_application_overhaul/migration.sql
@@ -13,6 +13,17 @@
   - Added the required column `userId` to the `SingleUseKey` table without a default value. This is not possible if the table is not empty.
 
 */
+-- AlterEnum
+-- This migration adds more than one value to an enum.
+-- With PostgreSQL versions 11 and earlier, this is not possible
+-- in a single migration. This can be worked around by creating
+-- multiple migrations, each migration adding only one value to
+-- the enum.
+
+
+ALTER TYPE "Role" ADD VALUE 'MENTOR';
+ALTER TYPE "Role" ADD VALUE 'UNDECLARED';
+
 -- DropForeignKey
 ALTER TABLE "AuthKey" DROP CONSTRAINT "AuthKey_user_id_fkey";
 
@@ -51,7 +62,10 @@ ALTER TABLE "AuthUser" ADD COLUMN     "githubId" INTEGER,
 ADD COLUMN     "githubUsername" TEXT,
 ADD COLUMN     "goodleUsername" TEXT,
 ADD COLUMN     "googleId" TEXT,
-ADD COLUMN     "password" TEXT;
+ADD COLUMN     "hashedPassword" TEXT;
+
+-- AlterTable
+ALTER TABLE "Question" ADD COLUMN     "targetGroup" TEXT[];
 
 -- AlterTable
 ALTER TABLE "SingleUseKey" DROP COLUMN "user_id",
