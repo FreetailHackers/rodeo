33c3f21ebe5d3b6a6d66b94c2886566a43726438
brandonShin-04 <brandonshin@utexas.edu>
2025-10-13 21:11:24 -0500

    squashed migrations

--- prisma/migrations/20251014021003_added_groups_model/migration.sql
+++ prisma/migrations/20251014021003_added_groups_model/migration.sql
@@ -16,21 +16,8 @@ CREATE TABLE "Group" (
     CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Blacklist" (
-    "id" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "value" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Blacklist_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "Group_id_key" ON "Group"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Blacklist_type_value_key" ON "Blacklist"("type", "value");
 
 -- AddForeignKey
 ALTER TABLE "User" ADD CONSTRAINT "User_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("id") ON DELETE SET NULL ON UPDATE CASCADE;
